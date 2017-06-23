e3d2469b900206224291f689d5843841e5a08f2f
Yesmith Tamayo <yestamayo@iMac-de-Yesmith.local>
2017-06-23 09:23:11 -0500

    Orden de las carpetas

--- server/main.js
+++ server/main.js
@@ -1,50 +1,31 @@
 var express = require('express');  
 var app = express();  
 var server = require('http').Server(app);  
-var io = require('socket.io')(server); 
+var io = require('socket.io')(server);
 
 var messages = [{  
-    author: "Carlos",
-    text: "Hola! que tal?"
-},{
-    author: "Pepe",
-    text: "Muy bien! y tu??"
-},{
-    author: "Paco",
-    text: "Genial!"
+  id: 1,
+  text: "Hola soy un mensaje",
+  author: "Carlos Azaustre"
 }];
 
-app.use(express.static('public'));  
+app.use(express.static('public'));
 
-//Funcion que esta atenta a las conexiones de los clientes
+app.get('/hello', function(req, res) {  
+  res.status(200).send("Hello World!");
+});
+
 io.on('connection', function(socket) {  
-    console.log('Un cliente se ha conectado');
-    socket.emit('messages', messages);
-});
+  console.log('Alguien se ha conectado con Sockets');
+  socket.emit('messages', messages);
 
-//Cliente
-var socket = io.connect('http://localhost:8080', { 'forceNew': true });
+  socket.on('new-message', function(data) {
+    messages.push(data);
 
-socket.on('messages', function(data) {  
-    console.log(data);
-});
-
-//Muestra la información del array de mensajes
-function render(data) {  
-    var html = data.map(function(elem, index){
-        return(`<div>
-                 <strong>${elem.author}</strong>:
-                 <em>${elem.text}</em>
-        </div>`)
-    }).join(" ");
-
-    document.getElementById('messages').innerHTML = html;
-}
-
-socket.on('messages', function(data) {  
-    render(data);
+    io.sockets.emit('messages', messages);
+  });
 });
 
 server.listen(8080, function() {  
-    console.log('Servidor corriendo en http://localhost:8080');
+  console.log("Servidor corriendo en http://localhost:8080");
 });
